dc42b26af86135a90edf96c18cd6005a72ac1d93
mars-1002 <matthewabsalonramirez@gmail.com>
2023-05-15 23:18:18 -0400

    init middleware

--- src/services/outfitService.js
+++ src/services/outfitService.js
@@ -4,6 +4,23 @@ import * as tokenService from './tokenService'
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/outfits`
 
 
+
+async function create(outfitFormData){
+  try{
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(outfitFormData)
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
 async function index() {
   try {
     const res = await fetch(BASE_URL, {
@@ -14,7 +31,79 @@ async function index() {
     console.log(err)
   }
 }
+async function show(outfitId){
+  try{
+    const res = await fetch(`${BASE_URL}/${outfitId}`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
+async function update(outfitFormData){
+  try{
+    const res = await fetch(`${BASE_URL}/${outfitFormData._id}`, {
+      method: 'PUT',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blogFormData)
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
+async function createComment(outfitId, commentFormData){
+  try{
+    const res = await fetch(`${BASE_URL}/${outfitId}/comments`, {
+      method: 'POST',
+      headers: { 
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(commentFormData)
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
+async function deleteOutfit(outfitId){
+  try{
+    const res = await fetch(`${BASE_URL}/${outfitId}`, {
+      method: 'DELETE',
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
+async function deleteComment(outfitId, commentId){
+  try{
+    const res = await fetch(`${BASE_URL}/${outfitId}/${commentId}`, {
+      method: 'DELETE'
+    })
+    return res.json()
+  }catch (err) {
+    console.log(err)
+  }
+}
+
 
 export {
+  create,
   index,
+  show,
+  update,
+  createComment,
+  deleteOutfit as delete,
+  deleteComment,
 }
