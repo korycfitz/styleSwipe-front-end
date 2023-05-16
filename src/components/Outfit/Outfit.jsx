f6bf2360a759315972c39b94efeae190ca93629b
Christian Musto <christianmustoj@gmail.com>
2023-05-16 18:57:28 -0400

    stub icons page correctly, rename directory, and implement component into outfit component

--- src/components/Outfit/Outfit.jsx
+++ src/components/Outfit/Outfit.jsx
@@ -6,6 +6,7 @@ import OutfitCard from '../OutfitCard/OutfitCard'
 import Comments from '../Comments/Comments'
 import NewComment from '../NewComment/NewComment'
 import AuthorInfo from '../AuthorInfo/AuthorInfo'
+import OutfitIcons from '../OutfitIcons/OutfitIcons'
 
 const Outfit = (props) => {
   return (
@@ -14,6 +15,7 @@ const Outfit = (props) => {
         {props.outfits.map((outfit, idx) => (
           <OutfitCard key={idx} outfit={outfit} user={props.user}/>
         ))}
+        <OutfitIcons />
         <AuthorInfo />
         <OutfitCard />
         <Outfit />
