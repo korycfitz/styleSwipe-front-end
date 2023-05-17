6114292b9bccb50d5c5974ae08cb20bd1305d7d4
RT527 <rafi.talukder7@gmail.com>
2023-05-17 09:15:52 -0400

    Update New Outfit Page

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
       setOutfits(data)
     }
     if (user) fetchAllOutfits()
-  }, [user])
+  }, [user, outfits])
   
 
 
--- src/pages/NewOutfit/NewOutfit.jsx
+++ src/pages/NewOutfit/NewOutfit.jsx
@@ -1,7 +1,72 @@
+import { useState, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const NewOutfit = () => {
+  const navigate = useNavigate()
+  const imgInputRef = useRef(null)
+
+  const [photo, setPhoto] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleChangePhoto = (e) => {
+    const file = e.target.files[0]
+    setPhoto(file)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      if (!import.meta.env.VITE_BACK_END_SERVER_URL) {
+        throw new Error('No VITE_BACK_END_SERVER_URL in front-end .env')
+      }
+
+      const formData = new FormData()
+      formData.append('photo', photo)
+      formData.append('description', description)
+
+      const response = await fetch('/api/outfits', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (response.ok) {
+        navigate('/')
+      } else {
+        const data = await response.json()
+        throw new Error(data.error)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
-    <>
-    </>
+    <div className="new-outfit">
+      <h2>Create a New Outfit</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="photo">Photo:</label>
+          <input
+            type="file"
+            id="photo"
+            accept="image/*"
+            onChange={handleChangePhoto}
+            ref={imgInputRef}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
   )
 }
 
