6e9ecee71cd27092705bdfe4ca3c3b1d40987623
Christian Musto <christianmustoj@gmail.com>
2023-05-17 04:45:03 -0400

    remove unnecessary message

--- src/components/OutfitPreview/OutfitPreview.jsx
+++ src/components/OutfitPreview/OutfitPreview.jsx
@@ -5,7 +5,6 @@ const OutfitPreview = (props) => {
   return (
     <div className={styles.container}>
       <section>
-        <h5>change &lt;img&gt; src value to correct props:</h5>
       <img className={styles.outfitPost} src={props.outfit.photo} alt="The user's outfit" />
       </section>
     </div>
