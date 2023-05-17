1e488bd23ac5ea6233b1e890bdb40e9ab99e5521
korycfitz <korycfitz@gmail.com>
2023-05-17 11:36:18 -0400

    fix signup redirect

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -65,7 +65,7 @@ const Signup = ({ handleAuthEvt }) => {
       handleAuthEvt()
       //this is the correct navigation, but we need to pick a random outfit to navigate to, and navigate to a specific location if no outfits exist, that the user has not swiped on
       // navigate(`/outfits/${outfitId}`)
-      navigate(`/outfits/6463c2044545ecb14bd59823`) //this is hard coded in
+      navigate(`/outfits/6464ddb3b1cb7ba415749d1e`) //this is hard coded in
     } catch (err) {
       console.log(err)
       setMessage(err.message)
