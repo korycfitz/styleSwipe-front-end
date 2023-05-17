331ad062477069b4d522331ddb65ad6fb77b772e
RT527 <rafi.talukder7@gmail.com>
2023-05-17 14:27:29 -0400

    Update profile cards

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -5,7 +5,7 @@ import * as profileService from '../../services/profileService'
 
 // css
 import styles from './Profiles.module.css'
-
+import fake from "../../assets/no_userprofile_picture.png"
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
 
@@ -25,14 +25,21 @@ const Profiles = () => {
     <main className={styles.container}>
       <h1>Hello!! This is a list of all the profiles.</h1>
       <div className={styles.profileList}>
-        {profiles.map(profile => (
-          <div className={styles.profileBox} key={profile._id}>
-            <img src={profile.photo} alt={profile.name} className={styles.profileImage} />
+        {profiles.map((profile) => (
+          <div
+            className={styles.profileBox}
+            key={profile._id}
+            style={{ backgroundColor: profile.photo ? '' : '#f2f2f2' }}
+          >
+            <img
+              src={profile.photo || fake }
+              alt={profile.name}
+              className={styles.profileImage}
+            />
             <p className={styles.profileName}>{profile.name}</p>
           </div>
         ))}
       </div>
-      
     </main>
   )
 }
