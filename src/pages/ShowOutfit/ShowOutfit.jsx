dc1f669d8680e1b15e88e77eaa5c2e5984d430ee
Christian Musto <christianmustoj@gmail.com>
2023-05-17 00:52:00 -0400

    showOutfit now maps and displays information

--- src/pages/ShowOutfit/ShowOutfit.jsx
+++ src/pages/ShowOutfit/ShowOutfit.jsx
@@ -3,13 +3,21 @@ import Outfit from "../../components/Outfit/Outfit"
 import Comments from "../../components/Comments/Comments"
 import NewComment from "../../components/NewComment/NewComment"
 
+//services
+import * as profileService from '../../services/profileService'
+
 const ShowOutfit = (props) => {
   return (
     <>
       {props.outfits.map((outfit) => (
         <div>
-          {/* ADD KEY HERE */}
-        <h1>{outfit.author.name}</h1>
+          <>
+            {/* ADD KEY HERE */}
+            {console.log(outfit)}
+          <h1>{outfit.author.name}</h1>
+          <h2>{outfit.description}</h2>
+          <h2>{outfit.author.photo}</h2>
+        </>
         </div>
       ))}
     </>
