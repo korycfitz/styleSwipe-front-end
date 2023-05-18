45408db09d0275106ca574a4d2a0d0934baef5e9
RT527 <rafi.talukder7@gmail.com>
2023-05-17 20:46:33 -0400

    update logo

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,6 @@ import logo from '../../assets/landing_logo.png'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h2 className={styles.welcomeMessage}>Hello, {user ? user.name : 'friend'}</h2>
       <img src={logo} alt="Landing Logo" className={styles.logo} />
     </main>
   )
