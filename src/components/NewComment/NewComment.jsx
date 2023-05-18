0084ca13044f590efb262395544786872fd23e7c
RT527 <rafi.talukder7@gmail.com>
2023-05-18 14:05:14 -0400

    update file

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -2,12 +2,36 @@
 import styles from './NewComment.module.css'
 
 
-const NewComment = () => {
+const NewComment = (props) => {
+  const [formData, setFormData] = useState({ text: '' })
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    props.handleAddComment(formData)
+    // last 
+    setFormData({ text: ''})
+  }
+
   return (
-    <div className={styles.container}>
-      
-    </div>
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <textarea
+        required
+        type="text"
+        name="text"
+        id="text-input"
+        value={formData.text}
+        placeholder="Add a Comment"
+        onChange={handleChange}
+      />
+      <button type="submit"><Icon category="Create" /></button>
+    </form>
   )
 }
+ 
+export default NewComment;
 
-export default NewComment
+// touched by Rafi
