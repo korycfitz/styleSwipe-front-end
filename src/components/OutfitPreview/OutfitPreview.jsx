d1837e3c39b1cff7b5cbb0843bb8312f80af1abb
korycfitz <korycfitz@gmail.com>
2023-05-17 21:07:21 -0400

    fix drilling props

--- src/components/OutfitPreview/OutfitPreview.jsx
+++ src/components/OutfitPreview/OutfitPreview.jsx
@@ -1,12 +1,13 @@
 // css
 import styles from './OutfitPreview.module.css'
 
-const OutfitPreview = (props) => {
+const OutfitPreview = ({outfit}) => {
+  console.log(outfit, 'outfit')
   return (
     <div className={styles.container}>
-      <section>
-      <img className={styles.outfitPost} src={props.outfit.photo} alt="The user's outfit" />
-      </section>
+      {/* <section>
+        <img className={styles.outfitPost} src={outfit.photo} alt="The user's outfit" />
+      </section> */}
     </div>
   )
 }
