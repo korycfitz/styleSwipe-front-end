f95955aa64590a93f7dae9d5f3f0a8a66a2268c0
RT527 <rafi.talukder7@gmail.com>
2023-05-19 01:45:02 -0400

    Profile update

---
+++ src/pages/Profiles/Profiles.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import * as profileService from '../../services/profileService'
+import styles from './Profiles.module.css'
+import fake from '../../assets/no_userprofile_picture.png'
+import pictureFrameImage from '../../assets/picture_frame.png'
+
+const Profiles = () => {
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const profileData = await profileService.getAllProfiles()
+      setProfiles(profileData)
+    }
+    fetchProfiles()
+  }, [])
+
+  if (!profiles.length) {
+    return (
+      <main className={styles.container}>
+        <h1>Loading...</h1>
+      </main>
+    )
+  }
+
+  return (
+    <main className={styles.container}>
+      <h1>All Profiles</h1>
+      <div className={styles.profileList}>
+        {profiles.map((profile) => (
+          <div className={styles.profileBox} key={profile._id}>
+            <div className={styles.pictureFrame}>
+              <img
+                src={pictureFrameImage}
+                alt="Picture Frame"
+                className={styles.pictureFrameImage}
+              />
+              <img
+                src={profile.photo || fake}
+                alt={profile.name}
+                className={styles.profileImage}
+              />
+            </div>
+            <p className={styles.profileName}>{profile.name}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default Profiles
