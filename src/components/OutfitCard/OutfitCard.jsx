6681caf1ef6e0695f1b2bae89469db28b8430e9f
Christian Musto <christianmustoj@gmail.com>
2023-05-19 04:32:09 -0400

    update README

--- src/components/OutfitCard/OutfitCard.jsx
+++ src/components/OutfitCard/OutfitCard.jsx
@@ -13,9 +13,9 @@ const OutfitCard = ({ outfit }) => {
       <article className={styles.container}>
         <header>
           <span>
-            <h1>
+
               <img src={outfit.photo} alt="Outfit" width="100" height="100" />
-            </h1>
+
           </span>
           <AuthorInfo content={outfit} />
         </header>
